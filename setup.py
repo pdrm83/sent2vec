9fe02e5f45c020157302ad351ebe0cb19f8259ad
Pedram Ataee <pedram.ataee@gmail.com>
2021-09-21 00:18:07 -0700

    Release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="sent2vec",
-    version="0.2.0",
+    version="0.2.1",
     description="How to encode sentences in a high-dimensional vector space, a.k.a., sentence embedding.",
     long_description=README,
     long_description_content_type="text/markdown",
