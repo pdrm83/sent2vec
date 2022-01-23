7b3be6ad131e4d299bb335886ab1bd58fdaf3a18
Alberto Marengo <marengo.albert@gmail.com>
2022-01-23 15:27:17 -0800

    Allowed for GPU implementation on vectorizer and fixed splitter SpaCy import

--- sent2vec/vectorizer.py
+++ sent2vec/vectorizer.py
@@ -10,11 +10,16 @@ class Vectorizer:
         self.vectors = []
 
     def bert(self, sentences, pretrained_weights='distilbert-base-uncased'):
+        # Checks if cuda is available and assigns device
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print(f'Vectorization done on {device} device')
         model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel,
                                                             ppb.DistilBertTokenizer,
                                                             pretrained_weights)
         tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+        # Move model to device
         model = model_class.from_pretrained(pretrained_weights)
+        model = model.to(device)
         tokenized = list(map(lambda x: tokenizer.encode(x, add_special_tokens=True), sentences))
 
         max_len = 0
@@ -23,13 +28,15 @@ class Vectorizer:
                 max_len = len(i)
 
         padded = np.array([i + [0] * (max_len - len(i)) for i in tokenized])
-        input_ids = torch.tensor(np.array(padded)).type(torch.LongTensor)
+        # Move inputs to same device as model
+        input_ids = torch.tensor(np.array(padded)).type(torch.LongTensor).to(device)
         # attention_mask = torch.tensor(np.where(padded != 0, 1, 0)).type(torch.LongTensor)
 
         with torch.no_grad():
             last_hidden_states = model(input_ids)
 
-        vectors = last_hidden_states[0][:, 0, :].numpy()
+        # Move vector results back to cpu if calculation was done on GPU
+        vectors = last_hidden_states[0][:, 0, :].cpu().numpy()
         self.vectors = vectors
 
     def word2vec(self, words, pretrained_vectors_path, ensemble_method='average'):
