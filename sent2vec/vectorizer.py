54e5ef50c8f562bae82399ae208097b4eb8f26a7
Alberto Marengo <marengo.albert@gmail.com>
2022-02-21 14:45:30 -0800

    First pass to change project architecture

--- sent2vec/vectorizer.py
+++ sent2vec/vectorizer.py
@@ -6,7 +6,7 @@ import transformers as ppb
 
 
 class Vectorizer:
-    def __init__(self, model='bert', pretrained_weights='distilbert-base-uncased', pretrained_vectors_path=None):
+    def __init__(self, model='bert', pretrained_weights='distilbert-base-uncased', pretrained_vectors_path=None, ensemble_method='average'):
         self.vectors = []
         self.use_bert = True
         if model == 'bert':
@@ -19,16 +19,53 @@ class Vectorizer:
             self.model = model_class.from_pretrained(pretrained_weights)
         elif model == 'word2vec':
             self.use_bert = False
+            self.ensemble_method = ensemble_method
             _, file_extension = os.path.splitext(pretrained_vectors_path)
             # Checks if file extension is binary
             if file_extension == '.bin':
                 self.model = gensim.models.KeyedVectors.load_word2vec_format(pretrained_vectors_path, binary=True)
             elif file_extension == '.txt':
-                self.model = gensim.models.KeyedVectors.load_word2vec_format(pretrained_vectors_path, ensemble_method='average')
+                self.model = gensim.models.KeyedVectors.load_word2vec_format(pretrained_vectors_path)
             else:
                 raise IOError(f'The file extension {file_extension} is not valid. Word2vec valid formats are ".txt" and ".bin".')
         else:
             raise  NameError(f'Wrong model name {model} passed.')
+
+    def vectorize(self, input_text):
+        if self.use_bert:
+            sentences = input_text
+            model = self.model.to(self.device)
+            model.eval()
+            tokenized = list(map(lambda x: self.tokenizer.encode(x, add_special_tokens=True), sentences))
+
+            max_len = 0
+            for i in tokenized:
+                if len(i) > max_len:
+                    max_len = len(i)
+
+            padded = np.array([i + [0] * (max_len - len(i)) for i in tokenized])
+            # Move inputs to same device as model
+            input_ids = torch.tensor(np.array(padded)).type(torch.LongTensor).to(self.device)
+            # attention_mask = torch.tensor(np.where(padded != 0, 1, 0)).type(torch.LongTensor)
+
+            with torch.no_grad():
+                last_hidden_states = model(input_ids)
+
+            # Move vector results back to cpu if calculation was done on GPU
+            vectors = last_hidden_states[0][:, 0, :].cpu().numpy()
+            self.vectors = vectors
+        else:
+            words = input_text
+            vectors = []
+            for element in words:
+                temp = []
+                for w in element:
+                    temp.append(self.model[w])
+                if self.ensemble_method == 'average':
+                    vectors.extend([np.mean(temp, axis=0)])
+
+            self.vectors = vectors
+
 
 
 
