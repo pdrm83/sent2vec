10535037fb6c01203ea6c9a8a643e29054b2c169
Alberto Marengo <marengo.albert@gmail.com>
2022-03-06 15:11:28 -0800

    Final commit NEW achitecture

---
+++ sent2vec/modules.py
@@ -0,0 +1,94 @@
+import numpy as np
+import os
+import gensim
+import torch
+import transformers as ppb
+from sent2vec.splitter import *
+
+class BaseVectorizer():
+
+    def __init__(self, **kwargs):
+        self.pretrained_weights = kwargs.get('pretrained_weights')
+        self.pretrained_vectors_path = kwargs.get('pretrained_vectors_path')
+        self.ensemble_method = kwargs.get('ensemble_method', 'average')
+    
+    def load_model(self):
+        pass
+
+    def _check_inputs_(self, sentences):
+        for sentence in sentences:
+            if type(sentence) != str:
+                raise TypeError(f'All items must be string type but {sentence} is type {type(sentence)}.')
+
+
+class BertVectorizer(BaseVectorizer):
+
+    def __init__(self, pretrained_weights):
+        super().__init__(pretrained_weights=pretrained_weights)
+        self.load_model()
+    
+    def load_model(self):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print(f'Vectorization done on {self.device} device')
+        model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel,
+                                                            ppb.DistilBertTokenizer,
+                                                            self.pretrained_weights)
+        self.tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+        self.model = model_class.from_pretrained(pretrained_weights)
+    
+    def execute(self, sentences):
+        assert type(sentences) == list, 'A list must be passed!'
+        self._check_inputs_(sentences)
+        model = self.model.to(self.device)
+        model.eval()
+        tokenized = list(map(lambda x: self.tokenizer.encode(x, add_special_tokens=True), sentences))
+        max_len = 0
+        for i in tokenized:
+            if len(i) > max_len:
+                max_len = len(i)
+        padded = np.array([i + [0] * (max_len - len(i)) for i in tokenized])
+        # Move inputs to same device as model
+        input_ids = torch.tensor(np.array(padded)).type(torch.LongTensor).to(self.device)
+        # attention_mask = torch.tensor(np.where(padded != 0, 1, 0)).type(torch.LongTensor)
+        with torch.no_grad():
+            last_hidden_states = model(input_ids)
+        # Move vector results back to cpu if calculation was done on GPU
+        vectors = last_hidden_states[0][:, 0, :].cpu().numpy()
+        return vectors
+
+class Sent2vecVectorizer(BaseVectorizer):
+
+    def __init__(self, pretrained_vectors_path, ensemble_method):
+        super().__init__(pretrained_vectors_path=pretrained_vectors_path, ensemble_method=ensemble_method)
+        self.splitter = Splitter()
+        self.load_model()
+    
+    def load_model(self):
+        assert self.pretrained_vectors_path, 'Need to pass a valid path to load word2vec'
+        _, file_extension = os.path.splitext(self.pretrained_vectors_path)
+        # Checks if file extension is binary
+        if file_extension == '.bin':
+            self.model = gensim.models.KeyedVectors.load_word2vec_format(self.pretrained_vectors_path, binary=True)
+        elif file_extension == '.txt':
+            self.model = gensim.models.KeyedVectors.load_word2vec_format(self.pretrained_vectors_path)
+        else:
+            raise IOError(f'The file extension {file_extension} is not valid. Word2vec valid formats are ".txt" and ".bin".')
+    
+    def execute(self, sentences):
+        self.splitter.sent2words(sentences=sentences, remove_stop_words=['not'], add_stop_words=[])
+        words = self.splitter.words
+        vectors = []
+        for element in words:
+            temp = []
+            for w in element:
+                temp.append(self.model[w])
+            if self.ensemble_method == 'average':
+                element_vec = np.mean(temp, axis=0)
+                try:
+                        vectors.any()
+                except:
+                    vectors = element_vec
+                else:
+                    vectors = np.vstack([vectors, element_vec])
+        return vectors
+        
--- sent2vec/vectorizer.py
+++ sent2vec/vectorizer.py
@@ -1,133 +1,25 @@
-import numpy as np
-import os
-import gensim
-import torch
-import transformers as ppb
+from sent2vec.modules import *
 
 
 class Vectorizer:
     def __init__(self, model='bert', pretrained_weights='distilbert-base-uncased', pretrained_vectors_path=None, ensemble_method='average'):
         self.vectors = []
-        self.use_bert = True
         if model == 'bert':
             print(f'Initializing Bert {pretrained_weights}!')
-            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-            print(f'Vectorization done on {self.device} device')
-            model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel,
-                                                                ppb.DistilBertTokenizer,
-                                                                pretrained_weights)
-            self.tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-            self.model = model_class.from_pretrained(pretrained_weights)
+            self.vectorizer = BertVectorizer(pretrained_weights)
         elif model == 'word2vec':
             assert pretrained_vectors_path, 'Need to pass a valid path to load word2vec'
             print('Initializing word2vec!')
-            self.use_bert = False
-            self.ensemble_method = ensemble_method
-            _, file_extension = os.path.splitext(pretrained_vectors_path)
-            # Checks if file extension is binary
-            if file_extension == '.bin':
-                self.model = gensim.models.KeyedVectors.load_word2vec_format(pretrained_vectors_path, binary=True)
-            elif file_extension == '.txt':
-                self.model = gensim.models.KeyedVectors.load_word2vec_format(pretrained_vectors_path)
-            else:
-                raise IOError(f'The file extension {file_extension} is not valid. Word2vec valid formats are ".txt" and ".bin".')
+            self.vectorizer = Sent2vecVectorizer(pretrained_vectors_path, ensemble_method)
         else:
-            raise  NameError(f'Wrong model name {model} passed.')
+            raise  TypeError(f'Wrong model name {model} passed.')
 
-    def vectorize(self, input_text):
-
-        def assertion_check(is_bert, text):
-            if (is_bert and type(text[0]) == str) | (not is_bert and type(text[0]) == list):
-                return True
-            else:
-                return False
-
-        assert type(input_text) == list, 'A list must be passed!'
-        assert assertion_check(self.use_bert, input_text), 'Wrong inputs to the model!'
-        if self.use_bert:
-            sentences = input_text
-            model = self.model.to(self.device)
-            model.eval()
-            tokenized = list(map(lambda x: self.tokenizer.encode(x, add_special_tokens=True), sentences))
-
-            max_len = 0
-            for i in tokenized:
-                if len(i) > max_len:
-                    max_len = len(i)
-
-            padded = np.array([i + [0] * (max_len - len(i)) for i in tokenized])
-            # Move inputs to same device as model
-            input_ids = torch.tensor(np.array(padded)).type(torch.LongTensor).to(self.device)
-            # attention_mask = torch.tensor(np.where(padded != 0, 1, 0)).type(torch.LongTensor)
-
-            with torch.no_grad():
-                last_hidden_states = model(input_ids)
-
-            # Move vector results back to cpu if calculation was done on GPU
-            vectors = last_hidden_states[0][:, 0, :].cpu().numpy()
-            self.vectors = vectors
-        else:
-            words = input_text
-            vectors = []
-            for element in words:
-                temp = []
-                for w in element:
-                    temp.append(self.model[w])
-                if self.ensemble_method == 'average':
-                    vectors.extend([np.mean(temp, axis=0)])
-
-            self.vectors = vectors
+    def run(self, sentences):
+        vectors = self.vectorizer.execute(sentences)
+        for idx in range(vectors.shape[0]):
+            self.vectors.append(vectors[idx])
+            
 
 
 
 
-    def bert(self, sentences, pretrained_weights='distilbert-base-uncased'):
-        # Checks if cuda is available and assigns device
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        print(f'Vectorization done on {device} device')
-        model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel,
-                                                            ppb.DistilBertTokenizer,
-                                                            pretrained_weights)
-        tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-        model = model_class.from_pretrained(pretrained_weights)
-        # Move model to device
-        model = model.to(device)
-        model.eval()
-        tokenized = list(map(lambda x: tokenizer.encode(x, add_special_tokens=True), sentences))
-
-        max_len = 0
-        for i in tokenized:
-            if len(i) > max_len:
-                max_len = len(i)
-
-        padded = np.array([i + [0] * (max_len - len(i)) for i in tokenized])
-        # Move inputs to same device as model
-        input_ids = torch.tensor(np.array(padded)).type(torch.LongTensor).to(device)
-        # attention_mask = torch.tensor(np.where(padded != 0, 1, 0)).type(torch.LongTensor)
-
-        with torch.no_grad():
-            last_hidden_states = model(input_ids)
-
-        # Move vector results back to cpu if calculation was done on GPU
-        vectors = last_hidden_states[0][:, 0, :].cpu().numpy()
-        self.vectors = vectors
-
-    def word2vec(self, words, pretrained_vectors_path, ensemble_method='average'):
-        _, file_extension = os.path.splitext(pretrained_vectors_path)
-        # Checks if file extension is binary
-        if file_extension == '.bin':
-            model = gensim.models.KeyedVectors.load_word2vec_format(pretrained_vectors_path, binary=True)
-        elif file_extension == '.txt':
-            model = gensim.models.KeyedVectors.load_word2vec_format(pretrained_vectors_path)
-        else:
-            raise IOError(f'The file extension {file_extension} is not valid. Word2vec valid formats are ".txt" and ".bin".')
-
-        vectors = []
-        for element in words:
-            temp = []
-            for w in element:
-                temp.append(model[w])
-            if ensemble_method == 'average':
-                vectors.extend([np.mean(temp, axis=0)])
-
-        self.vectors = vectors
